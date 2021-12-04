108badfa1593ce49fe1c41f81953646031a8758a
sonotype <contact.marco.accardi@gmail.com>
2021-12-04 17:15:22 +0100

    update navbar

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 
-import { Container, Wrapper, Left, Center, Right } from "./navbar_style";
+import {
+  Container,
+  Wrapper,
+  Left,
+  Center,
+  Right,
+  Language,
+  SearchContainer,
+  Input,
+  Logo,
+  MenuItem,
+} from "./navbar_style";
+import { Search, ShoppingCartOutlined } from "@mui/icons-material/";
+import { Badge } from "@mui/material/";
 
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
-        <Left></Left>
-        <Center></Center>
-        <Right></Right>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input></Input>
+            <Search style={{ color: "grey", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>LAMA</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
