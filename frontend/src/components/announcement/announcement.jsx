b9366983b4815b6d9bce83061545182ac80d5066
sonotype <contact.marco.accardi@gmail.com>
2021-12-04 17:15:04 +0100

    add announcement component

---
+++ frontend/src/components/announcement/announcement.jsx
@@ -0,0 +1,7 @@
+import { Container } from "./announcement_style";
+
+const Announcement = () => {
+  return <Container>Super Deal! Free Shipping on Orders Over $50</Container>;
+};
+
+export default Announcement;
