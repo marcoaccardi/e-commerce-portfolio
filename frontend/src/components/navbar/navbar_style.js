5642033a35deb612cb946db4b722257b57dd05a0
sonotype <contact.marco.accardi@gmail.com>
2021-12-09 21:08:16 +0100

    init responsivness

--- frontend/src/components/navbar/navbar_style.js
+++ frontend/src/components/navbar/navbar_style.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
+import { mobile } from "../../responsive";
 export const Container = styled.div`
   height: 60px;
   background-color: black;
+  ${mobile({ height: "50px " })}
 `;
 
 export const Wrapper = styled.div`
