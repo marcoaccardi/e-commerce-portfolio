fd8b1776e777a5482bf9dafdcb4d2c53d8a7ae46
sonotype <contact.marco.accardi@gmail.com>
2021-12-10 09:15:51 +0100

    add password encryption and login auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,47 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //REGISTER
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
 
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+    console.log(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    //evaluate if it is falsy(false, null, undefined, '', 0 or NaN)
+    !user && res.status(401).json("wrong credentials");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("wrong credentials");
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
   }
