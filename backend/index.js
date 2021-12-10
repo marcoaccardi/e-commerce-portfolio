09faf1017dc3b8797acfdb3cc8804c26cc8b8f8e
sonotype <contact.marco.accardi@gmail.com>
2021-12-10 06:58:52 +0100

    add user auth for testing postman and mongoDB

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 mongoose
@@ -13,6 +14,7 @@ mongoose
     console.log(err);
   });
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
-const moongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-const UserSchema = new moongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
