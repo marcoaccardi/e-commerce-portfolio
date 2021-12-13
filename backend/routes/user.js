e8668c1e6ddb203e55130efc0ddb9adb5ab1aac7
sonotype <contact.marco.accardi@gmail.com>
2021-12-13 15:30:06 +0100

    add get all user and user stats

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const {
-  verifyToken,
-  verifyTokenAndAuthorization,
-  verifyTokenAndAdmin,
-} = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
 
 // UPDATE USER
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
@@ -40,52 +36,11 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
 });
 
 // GET
-router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+router.get("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     const { password, ...others } = user._doc;
     res.status(200).json(others);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-// GET  ALL USERS
-router.get("/", verifyTokenAndAdmin, async (req, res) => {
-  const query = req.query.new;
-  try {
-    const users = query
-      ? await User.find().sort({ _id: -1 }).limit(2)
-      : await User.find();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// GET USERS STATS
-
-router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-  const date = new Date();
-  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-
-  try {
-    const data = await User.aggregate([
-      {
-        $match: { createdAt: { $gte: lastYear } },
-      },
-      {
-        $project: {
-          month: { $month: "$createdAt" },
-        },
-      },
-      {
-        $group: {
-          _id: "$month",
-          total: { $sum: 1 },
-        },
-      },
-    ]);
-    res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);
   }
